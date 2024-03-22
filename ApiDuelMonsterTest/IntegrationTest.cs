286c6cf39098d35806a807e322c4cf785aab5e7c
Manitob1 <damien.khashan@ifosup.wavre.be>
2024-03-22 22:38:03 +0100

    ajout test integration

--- ApiDuelMonsterTest/IntegrationTest.cs
+++ ApiDuelMonsterTest/IntegrationTest.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ApiDuelMonster.Data;
+using ApiDuelMonster.Pages;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ApiDuelMonsterTest
 {
-	internal class IntegrationTest
+	public class IntegrationTest
 	{
+		TestContext testContext = new TestContext();
+
+		[Fact]
+		public async Task TestGetCard()
+		{
+			//Arrange
+			testContext.JSInterop.Mode = JSRuntimeMode.Loose;
+			testContext.Services.AddSingleton<CardApi>();
+
+			var cut = testContext.RenderComponent<ApiDuelMonster.Pages.Index>();
+
+			//Act
+			cut.Find("button").Click();
+			await Task.Delay(2000);
+			cut.Render();
+
+			//Assert
+			var paragraphCount = cut.FindAll("p").Count();
+			Assert.True(paragraphCount > 2);
+		}
+
+
 	}
 }
